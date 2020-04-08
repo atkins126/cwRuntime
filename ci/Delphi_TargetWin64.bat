2c2fbad0c4d89e0bf7d766740761cdf6c9055d31
Craig Chapman <craig@chapmanworld.com>
2020-04-07 20:20:55 -0600

    Consolidated unit tests.

--- ci/Delphi_TargetWin64.bat
+++ ci/Delphi_TargetWin64.bat
@@ -16,22 +16,12 @@ msbuild %WORKSPACE%\src\projects\lib_cwLog_Rio.dproj                      || exi
 
 REM ------------------------------ Build test projects --------------------------
 
-msbuild %WORKSPACE%\src\tests\cwCollections\test_cwCollections_%VARIANT%.dproj  || exit /b
-msbuild %WORKSPACE%\src\tests\cwIO\test_cwIO_%VARIANT%.dproj                    || exit /b
 msbuild %WORKSPACE%\src\tests\cwLog\test_dynamic_cwLog_%VARIANT%.dproj          || exit /b
-msbuild %WORKSPACE%\src\tests\cwLog\test_static_cwLog_%VARIANT%.dproj           || exit /b
 msbuild %WORKSPACE%\src\tests\cwTest\test_cwTest_%VARIANT%.dproj                || exit /b
-msbuild %WORKSPACE%\src\tests\cwTypes\test_cwTypes_%VARIANT%.dproj              || exit /b
-msbuild %WORKSPACE%\src\tests\cwUnicode\test_cwUnicode_%VARIANT%.dproj          || exit /b
-msbuild %WORKSPACE%\src\tests\cwVectors\test_cwVectors_%VARIANT%.dproj          || exit /b
+msbuild %WORKSPACE%\src\tests\cwRuntime\test_cwRuntime_%VARIANT%.dproj          || exit /b
 
 REM ------------------------------ Execute test projects --------------------------
 
-%WORKSPACE%\out\bin\Win64\Debug\test_cwCollections.exe                    || exit /b
-REM - %WORKSPACE%\out\bin\Win64\Debug\test_cwIO.exe                            || exit /b 
 %WORKSPACE%\out\bin\Win64\Debug\test_dynamic_cwLog.exe                    || exit /b
-%WORKSPACE%\out\bin\Win64\Debug\test_static_cwLog.exe                     || exit /b
 %WORKSPACE%\out\bin\Win64\Debug\test_cwTest.exe                           || exit /b
-%WORKSPACE%\out\bin\Win64\Debug\test_cwTypes.exe                          || exit /b
-%WORKSPACE%\out\bin\Win64\Debug\test_cwUnicode.exe                        || exit /b
-%WORKSPACE%\out\bin\Win64\Debug\test_cwVectors.exe                        || exit /b
+%WORKSPACE%\out\bin\Win64\Debug\test_cwRuntime.exe                        || exit /b
--- ci/LazWin64_TargetWin64.bat
+++ ci/LazWin64_TargetWin64.bat
@@ -13,21 +13,12 @@ lazbuild %WORKSPACE%\src\projects\lib_cwLog.lpi  || exit /b
 
 REM ------------------------------ Build test projects --------------------------
 
-lazbuild %WORKSPACE%\src\tests\cwCollections\test_cwCollections.lpi  || exit /b
-lazbuild %WORKSPACE%\src\tests\cwIO\test_cwIO.lpi                    || exit /b
 lazbuild %WORKSPACE%\src\tests\cwLog\test_dynamic_cwLog.lpi          || exit /b
-lazbuild %WORKSPACE%\src\tests\cwLog\test_static_cwLog.lpi           || exit /b
 lazbuild %WORKSPACE%\src\tests\cwTest\test_cwTest.lpi                || exit /b
-lazbuild %WORKSPACE%\src\tests\cwTypes\test_cwTypes.lpi              || exit /b
-lazbuild %WORKSPACE%\src\tests\cwUnicode\test_cwUnicode.lpi          || exit /b
+lazbuild %WORKSPACE%\src\tests\cwRuntime\test_cwRuntime.lpi          || exit /b
 
 REM ------------------------------ Execute test projects --------------------------
 
-%WORKSPACE%\out\bin\x86_64-win64\test_cwCollections.exe  || exit /b
-REM - %WORKSPACE%\out\bin\x86_64-win64\test_cwIO.exe           || exit /b 
 %WORKSPACE%\out\bin\x86_64-win64\test_dynamic_cwLog.exe  || exit /b
-%WORKSPACE%\out\bin\x86_64-win64\test_static_cwLog.exe   || exit /b
 %WORKSPACE%\out\bin\x86_64-win64\test_cwTest.exe         || exit /b
-%WORKSPACE%\out\bin\x86_64-win64\test_cwTypes.exe        || exit /b
-%WORKSPACE%\out\bin\x86_64-win64\test_cwUnicode.exe      || exit /b
-%WORKSPACE%\out\bin\x86_64-win64\test_cwVectors.exe      || exit /b
+%WORKSPACE%\out\bin\x86_64-win64\test_cwRuntime.exe      || exit /b
