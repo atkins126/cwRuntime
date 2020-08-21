5dbf7cda265d931bb637606df2ccd60e9576fce0
chapmanworld <craig@chapmanworld.com>
2020-08-21 16:10:21 -0600

    Adding cwThreading Long Threads sample to project groups

--- src/samples/cwThreading/03 - Long Threads/LongThreads.dpr
+++ src/samples/cwThreading/03 - Long Threads/LongThreads.dpr
@@ -26,7 +26,7 @@
   IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *)
 {$endif}
-program LongThreads_Lazarus_2_0_10;
+program LongThreads;
 uses
   sysutils //- for sleep()
 , cwThreading
